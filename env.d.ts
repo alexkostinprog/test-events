e3e25201d3eaa74af7382c02656448ec850e6cb1
Kostin Alex <alexkostin1@gmail.com>
2026-05-25 21:02:42 +0300

    Adds env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  // Описываем тип для нашего ключа погоды
+  readonly VITE_WEATHER_API_KEY: string
+  // Здесь можно описывать другие переменные, если они появятся
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
